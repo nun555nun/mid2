975a28fc2f06cfc31e3703db79e5eb5249f06fc8
nunmino <nun555nun@gmail.com>
2018-10-13 20:39:06 +0700

    hello

--- app/src/main/java/com/example/mid/SplashActivity.java
+++ app/src/main/java/com/example/mid/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.mid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent i = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(i);
     }
 }
